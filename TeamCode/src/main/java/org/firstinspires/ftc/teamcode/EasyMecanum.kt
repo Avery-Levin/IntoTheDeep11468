faddf8557a7e55b4941114a079f0e2a9f4c5f684
Avery Levin <averylev@gmail.com>
2024-10-01 16:42:41 -0400

    Added a Mecanum test OpMode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EasyMecanum.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EasyMecanum.kt
@@ -1,12 +1,16 @@
 package org.firstinspires.ftc.teamcode
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import org.baylorschool.intothedeep.lib.Mecanum
 
+@TeleOp
 class EasyMecanum : LinearOpMode() {
     val mecanum : Mecanum = Mecanum(hardwareMap)
     override fun runOpMode() {
-        mecanum.mecanumLoop(gamepad1 = gamepad1)
+        while(opModeIsActive()) {
+            mecanum.mecanumLoop(gamepad1 = gamepad1)
+        }
     }
 
 }
