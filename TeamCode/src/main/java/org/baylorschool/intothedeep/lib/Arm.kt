bbc171564c4d3d014eccd187f8ac89570fa143eb
Avery Levin <averylev@gmail.com>
2024-10-16 15:49:16 -0400

    Made actuator class abstract for concision
    This should make writing code for multiple different PID actuators a lot easier. This is completely untested, unfortunately, and I won't be able to test it for a while.

--- TeamCode/src/main/java/org/baylorschool/intothedeep/lib/Arm.kt
+++ TeamCode/src/main/java/org/baylorschool/intothedeep/lib/Arm.kt
@@ -1,75 +1,24 @@
-package org.firstinspires.ftc.teamcode.lib
+package org.baylorschool.intothedeep.lib
 
 import com.acmerobotics.dashboard.config.Config
-import com.qualcomm.robotcore.hardware.DcMotorEx
-import com.qualcomm.robotcore.hardware.Gamepad
 import com.qualcomm.robotcore.hardware.HardwareMap
-import org.firstinspires.ftc.robotcore.external.Telemetry
+import org.firstinspires.ftc.teamcode.lib.PIDActuator
 
-import org.baylorschool.intothedeep.controllers.PIDFController
-import org.baylorschool.intothedeep.controllers.PIDCoefficients
-import org.firstinspires.ftc.teamcode.lib.armPIDConfig.d
-import org.firstinspires.ftc.teamcode.lib.armPIDConfig.i
-import org.firstinspires.ftc.teamcode.lib.armPIDConfig.fg
-import org.firstinspires.ftc.teamcode.lib.armPIDConfig.target
-import org.firstinspires.ftc.teamcode.lib.armPIDConfig.p
+
 
 @Config
-object armPIDConfig {
-    @JvmField var p: Double = 0.0
-    @JvmField var i : Double = 0.0
-    @JvmField var d : Double = 0.0
-    @JvmField var fg: Double = 0.25
-    @JvmField var target: Double = 0.0
+
+class Arm(hardwareMap: HardwareMap) : PIDActuator(hardwareMap)
+{
+
+init {
+    var config = PIDConfig()
+    PIDConfig.p = 0.0
+    PIDConfig.i = 0.0
+    PIDConfig.d = 0.0
+    PIDConfig.fg = 0.0
+    PIDConfig.target = 0.0
+    motorName = "armMotor"
+    ticks_per_unit = 2786.2/360.0
 }
-class Arm(hardwareMap: HardwareMap) {
-
-    val ticks_per_degree = 2786.2 / 360.0
-    var correctedValue = target/ticks_per_degree
-    val armMotor: DcMotorEx
-    var armPos: Double = 0.0
-    private val pControl = PIDCoefficients(p, i, d)
-    private val controller = PIDFController(pControl)
-    var armPower = 0.0
-    private var offset = 0
-
-    init {
-        armMotor = hardwareMap.get(DcMotorEx::class.java, "armMotor")
-        offset = armMotor.currentPosition
-        armPos = armMotor.currentPosition.toDouble() - offset
-        target = 0.0
-    }
-
-    fun telemetry(telemetry: Telemetry) {
-        telemetry.addData("arm Motor Position", armPos)
-        telemetry.addData("Target Position", target)
-        telemetry.addData("power",armMotor.power)
-    }
-
-    fun update() {
-        correctedValue = target / ticks_per_degree
-        armPos = armMotor.currentPosition.toDouble() - offset
-        controller.targetPosition = target
-        armPower = controller.update(armPos) + ((Math.cos(Math.toRadians(target / ticks_per_degree))) * fg)
-    }
-
-    private fun increaseTarget() {
-
-        target += .4
-    }
-
-    private fun decreaseTarget() {
-        target -= .4
-    }
-
-    fun armLoop(gamepad: Gamepad) {
-        update()
-        armMotor.power = armPower
-
-        if (gamepad.dpad_up)
-            increaseTarget()
-        else if (gamepad.dpad_down)
-            decreaseTarget()
-    }
-
 }
