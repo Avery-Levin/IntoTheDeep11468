5f24bc6c35196199c42af050e24d1da9c853d82f
Avery-Levin <88396486+Avery-Levin@users.noreply.github.com>
2024-12-30 14:23:51 -0500

    Prepared change to library version of Pedro Pathing
    I've deleted the old version of Pedro, installed the library, and added references to the global class when necessary. The old follower constants and whatnot shouldn't be necessary with a new robot, but I want to run these by everybody before merging.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/constants/LConstants.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode.pedroPathing.constants;
+
+import com.pedropathing.localization.*;
+import com.pedropathing.localization.constants.*;
+
+import org.baylorschool.intothedeep.Global;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+public class LConstants {
+    static {
+        PinpointConstants.forwardY = Global.yOffset;
+        PinpointConstants.strafeX = Global.xOffset;
+        PinpointConstants.distanceUnit = DistanceUnit.INCH;
+        PinpointConstants.hardwareMapName = Global.odo;
+        PinpointConstants.useYawScalar = false;
+        PinpointConstants.yawScalar = 1.0;
+        PinpointConstants.useCustomEncoderResolution = false;
+        PinpointConstants.encoderResolution = GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_SWINGARM_POD;
+        PinpointConstants.customEncoderResolution = 13.26291192;
+        PinpointConstants.forwardEncoderDirection = GoBildaPinpointDriver.EncoderDirection.REVERSED; //TODO Determine the encoder direction for the final robot
+        PinpointConstants.strafeEncoderDirection = GoBildaPinpointDriver.EncoderDirection.FORWARD; //TODO Determine the encoder direction for the final robot
+
+
+    }
+}
+
+
+
+
