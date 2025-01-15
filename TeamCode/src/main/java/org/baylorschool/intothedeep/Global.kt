d823905424874f936c411932a70a0ea87fcad0f5
owen <owenpmckenna@outlook.com>
2025-01-15 09:32:28 -0500

    x and y offset

--- TeamCode/src/main/java/org/baylorschool/intothedeep/Global.kt
+++ TeamCode/src/main/java/org/baylorschool/intothedeep/Global.kt
@@ -13,8 +13,8 @@ object Global {
 
     // odo
     const val odo = "odo"
-    const val xOffset = 16.33858/2
-    const val yOffset = -8.464567//in
+    const val xOffset = 4.035
+    const val yOffset = 1.673
     const val mass = 5.44217
 
     // diffy
