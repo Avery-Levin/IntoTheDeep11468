78be8b061b64cdcb9dce9d8828f0a9aa464a0b32
Owen McKenna <owenpmckenna@outlook.com>
2025-02-01 15:54:04 -0500

    bad apple goes hard

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/ParkAuto.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/ParkAuto.kt
@@ -1,4 +1,5 @@
 package org.firstinspires.ftc.teamcode.auto
+import android.telephony.gsm.GsmCellLocation
 import com.acmerobotics.dashboard.FtcDashboard
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry
 import com.pedropathing.follower.Follower
@@ -22,22 +23,51 @@ import org.firstinspires.ftc.teamcode.pedroPathing.constants.LConstants
 
 @Autonomous
 class ParkAuto : LinearOpMode() {
+    private val placePreloadPos = Pose(10.0, 0.0, 0.0)
+    private val placePreloadPos1 = Pose(25.0, 0.0, 0.0)
+    private val push0StartPos = Pose(49.5, -46.5, 0.0)
+    private val push0BezierPosA = Pose(-7.5, -53.0, 0.0)//toward human player
+    private val push0BezierPosB = Pose(53.0, -23.5, 0.0)
+    private val push0EndPos = Pose(5.5, -46.5, 0.0)
+    private val push1BezierPosA = Pose(75.0, -46.5, 0.0)
+    private val push1StartPos = Pose(49.5, -56.5, 0.0)
+    private val push1EndPos = Pose(5.5, -56.5, 0.0)
+    private val push2BezierPosA = Pose(75.0, -52.0, 0.0)
+    private val push2StartPos = Pose(49.5, -62.0, 0.0)
+    private val push2EndPos = Pose(5.5, -62.0, 0.0)//47
+    private val pickup0Pos = Pose(12.0, -45.5, 0.0)
     override fun runOpMode() {
         Constants.setConstants(FConstants::class.java, LConstants::class.java)
-        val driver = Driver(Follower(hardwareMap), Pose(0.0, 0.0, 0.0))
-
+        val driver = Driver(Follower(hardwareMap), Pose(-2.0, 0.0, 0.0))
         val telemetryA = MultipleTelemetry(telemetry, FtcDashboard.getInstance().telemetry)
         val pivot = Pivot(hardwareMap)//up down
         val slides = Slides(hardwareMap)//forward back
         val depo = Depo(hardwareMap)//THE CLAW
         telemetryA.update()
         driver.update()
+        depo.set(Global.diffy180)
 
         waitForStart()
         ActionGroup(ActionSet(
-                driver.runToAction(Pose(1.0,12.0)),
+            Global.DiffyPosition.DiffySpecDepo.diffyPos.setAction(depo),
+            driver.runToAction(placePreloadPos),
+            Global.PivotPresets.RESET.action(pivot),
+            driver.runToAction(push0StartPos, push0BezierPosA, push0BezierPosB),
+            driver.runToAction(push0EndPos),
+            driver.runToAction(push1StartPos, push1BezierPosA),
+            driver.runToAction(push1EndPos),
+            driver.runToAction(push2StartPos, push2BezierPosA),
+            driver.runToAction(push2EndPos),
+            //ensureMinTime(driver.runToAction(pickup0Pos), 2000, true),
+            //driver.runToAction(Pose(1.0,12.0), Pose(10.0, 0.0)),
+            Global.PivotPresets.DEPO.action(pivot),
+            //Global.SlidePresets.
+            object : Action {
+                override fun init() {}
+                override fun update(): Boolean = false
+            }
         ), pivot.action(), slides.action(), driverAction(driver)).execute()
-        driver.holdPoint(Pose(2.0,12.0))
+        //driver.holdPoint(Pose(2.0,12.0))
 
         while (!isStopRequested && opModeIsActive()) {
             driver.update()
